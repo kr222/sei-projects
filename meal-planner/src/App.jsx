48d47fd2eeb3c13ba95d2370761d64515b919ea3
kr2 <rat.dog.girl@hotmail.com>
2024-03-15 01:23:12 +0800

    Tidy up App.jsx

--- meal-planner/src/App.jsx
+++ meal-planner/src/App.jsx
@@ -1,36 +1,38 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./index.css";
 import MacroTally from "./components/MacroTally";
 import TDEEDisplay from "./components/TDEEDisplay";
 import WeekPlan from "./components/WeekPlan";
+import Recipes from "./pages/Recipes";
 import NavBar from "./components/NavBar";
+import Planner from "./pages/Planner";
 
 function App() {
   return (
-    <React.StrictMode>
-      <div>
-        <h2>hello world</h2>
-        <div className="row">
-          <div className="col-sm-1"></div>
-          <div className="col-sm-2">
-            <TDEEDisplay></TDEEDisplay>
-          </div>
-          <div className="col-sm-8">
-            <WeekPlan></WeekPlan>
-          </div>
-          <div className="col-sm-1"></div>
+    <div>
+      <h2>hello world</h2>
+      <div className="row">
+        <div className="col-sm-1"></div>
+        <div className="col-sm-2">
+          <TDEEDisplay></TDEEDisplay>
         </div>
-        <div className="row">
-          <div className="col-sm-1"></div>
-          <br />
+        <div className="col-sm-8">
+          <WeekPlan></WeekPlan>
+        </div>
+        <div className="col-sm-1"></div>
+      </div>
+      <div className="row">
+        <br />
+        <div className="col-sm-1"></div>
+        <div className="col-sm-2">
           <NavBar></NavBar>
-          <div className="col-sm-8">
-            <MacroTally></MacroTally>
-            <div className="col-sm-1"></div>
-          </div>
+        </div>
+        <div className="col-sm-8">
+          <MacroTally></MacroTally>
         </div>
       </div>
-    </React.StrictMode>
+    </div>
   );
 }
 
