a78007193faf3090a72b8db0be17ff65d12050e6
kr2 <rat.dog.girl@hotmail.com>
2024-03-15 01:25:08 +0800

    Add NavBar.jsx

---
+++ meal-planner/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>nav 1</li>
+        <li>nav 2</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
