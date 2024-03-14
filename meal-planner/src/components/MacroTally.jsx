dd5ba6d7e86e91e366eec2d2db7c553562cb2b28
kr2 <rat.dog.girl@hotmail.com>
2024-03-15 01:24:16 +0800

    Redo MacroTally.jsx

---
+++ meal-planner/src/components/MacroTally.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MacroTally = () => {
+  return (
+    <>
+      <div>Macro Tally</div>
+      <table>
+        <tbody>
+          <tr>
+            <td>asdfasdfasdfas</td>
+            <td>asdfasdfasdfas</td>
+            <td>asdfasdfasdfas</td>
+            <td>asdfasdfasdfas</td>
+            <td>asdfasdfasdfas</td>
+            <td>asdfasdfasdfas</td>
+            <td>asdfasdfasdfas</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default MacroTally;
