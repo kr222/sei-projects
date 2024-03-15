9c1ee1e1e63dcd3ebb2d26e280b3e01166a3c609
kr2 <rat.dog.girl@hotmail.com>
2024-03-15 12:43:15 +0800

    Add edit functionality for MealItem

--- meal-planner/src/components/MealItem.jsx
+++ meal-planner/src/components/MealItem.jsx
@@ -1,41 +1,51 @@
 import React, { useEffect } from "react";
+import Edit from "./Edit";
+
 import { useState } from "react";
 
 const MealItem = (props) => {
-  const [itemDisplay, setItemDisplay] = useState("hi");
+  const [itemDisplay, setItemDisplay] = useState("meal");
+  const [showEdit, setShowEdit] = useState();
+
+  const dayMeals =
+    props.monBrek ||
+    props.tueBrek ||
+    props.wedBrek ||
+    props.monLun ||
+    props.tueLun ||
+    props.wedLun ||
+    props.monDin ||
+    props.tueDin ||
+    props.wedDin;
 
   const allocateMeals = () => {
-    setItemDisplay(
-      props.monBrek ||
-        props.tueBrek ||
-        props.wedBrek ||
-        props.monLun ||
-        props.tueLun ||
-        props.wedLun ||
-        props.monDin ||
-        props.tueDin ||
-        props.wedDin
-    );
+    setItemDisplay(dayMeals);
   };
   const handleMouseEnter = () => {
-    setItemDisplay("+");
+    setItemDisplay("edit");
   };
   const handleMouseLeave = () => {
     allocateMeals();
   };
 
-  const changeItemDisplay = () => {};
+  const handleClick = () => {
+    console.log(`hi ${dayMeals}`);
+    setShowEdit(true);
+  };
 
   return (
     <>
       <button
         className="col-sm-12"
         onMouseEnter={handleMouseEnter}
+        onClick={handleClick}
         onMouseLeave={handleMouseLeave}
       >
         {itemDisplay}
       </button>
-      <button onClick={allocateMeals}>state test</button>
+
+      <button>hello world</button>
+      {showEdit && <Edit edit={setShowEdit}></Edit>}
     </>
   );
 };
