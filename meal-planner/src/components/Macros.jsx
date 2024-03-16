843a9e0a93794445a352dd850337d9deed7f9c5e
kr2 <rat.dog.girl@hotmail.com>
2024-03-16 18:21:08 +0800

    Create map for select options

--- meal-planner/src/components/Macros.jsx
+++ meal-planner/src/components/Macros.jsx
@@ -19,10 +19,15 @@ const Macros = (props) => {
         <div className="col-sm-2">444</div>
         <br></br>
         <br></br>
-        <select className="col-sm-5">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
+        <select className="col-sm-5" id="options">
+          {props.recipes.map((item) => {
+            return (
+              // access option value by options.value
+              <option key={item.id} value={item.id}>
+                {item.fields.meal} {item.id}
+              </option>
+            );
+          })}
         </select>
         <div className="row">
           <button className="col" onClick={() => props.setShowMacros(false)}>
@@ -30,6 +35,9 @@ const Macros = (props) => {
           </button>
           <button className="col" onClick={() => props.setShowMacros(false)}>
             cancel
+          </button>
+          <button onClick={() => console.log(options.value)}>
+            test option value
           </button>
         </div>
       </div>
