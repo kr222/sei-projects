ed7cdebf16ef73e717c30676875bda388cb784ce
kr2 <rat.dog.girl@hotmail.com>
2024-03-16 18:22:02 +0800

    Add update meal funtionality

--- meal-planner/src/components/RecipeMeal.jsx
+++ meal-planner/src/components/RecipeMeal.jsx
@@ -30,6 +30,44 @@ const RecipeMeal = (props) => {
     }
   };
 
+  // function to update individual recipe
+  const updateMeal = async () => {
+    try {
+      const res = await fetch(
+        "https://api.airtable.com/v0/appeKmMdDs8azWyPx/recipes/" + props.id,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization:
+              "Bearer patnEBL5ICNyKDzQJ.7935cda5b01609b4bffa30b86e6d9e365a035dc1184d5fb2070c331817a54410",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fields: {
+              meal: "Overnight oats",
+              ingredients: "oats, milk, honey",
+              cal: "300",
+              pro: "34",
+              fat: "12",
+              carb: "20",
+            },
+          }),
+        }
+      );
+
+      if (res.ok) {
+        const data = await res.json();
+        console.log(`Meal updated successfully`);
+        props.getRecipes();
+        // setShowEdit(false);
+      }
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        console.log(error.message);
+      }
+    }
+  };
+
   // function to split ingredients and return an array to map into a list
   const listIngredients = () => {};
 
@@ -48,6 +86,7 @@ const RecipeMeal = (props) => {
         <h1 className="col-sm-2">{props.recipePro}</h1>
         <h1 className="col-sm-2">{props.recipeFat}</h1>
         <h1 className="col-sm-2">{props.recipeCarb}</h1>
+        <button onClick={updateMeal}>edit</button>
         <button onClick={deleteRecipe}>delete</button>
       </div>
     </>
